72f77d05ac0983d59b463478da7d613fec061866
Eva Kerrigan <evakerrigan@gmail.com>
2025-02-22 01:36:36 +0300

    refactor: fix resume path

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
+import resumePdf from "../assets/KseniiaLiubasova.pdf";
+
 export const Footer = () => {
   return (
     <div>
       <div className="footer-container">
-        <a className="button-download" href="KseniiaLiubasova.pdf">
+        <a className="button-download" href={resumePdf} download>
           <em>Скачать резюме</em>
         </a>
       </div>
