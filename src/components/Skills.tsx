0c568c5d62bbb85f4ae498fb15d2e7966ff8d057
Eva Kerrigan <evakerrigan@gmail.com>
2025-05-05 05:00:31 +0300

    refactor: refactor component skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,19 +1,16 @@
+import { skills } from "../data/skills";
+
 export const Skills = () => {
   return (
     <div className="skills-wrapper wrapper">
       <h2>Ключевые навыки</h2>
       <ul className="skills-list">
-        <li>TypeScript и JavaScript</li>
-        <li>
-          React.js для создания интерактивных пользовательских интерфейсов
-        </li>
-        <li>Redux для управления состоянием приложения</li>
-        <li>UI-фреймворк Ant Design для создания качественных компонентов</li>
-        <li>Chakra UI и Tailwind CSS для стилизации</li>
-        <li>Инструменты для проектирования: Figma и Photoshop</li>
-        <li>Контроль версий Git</li>
-        <li>Jira для эффективного управления проектами</li>
-        <li>Pixel Perfect для обеспечения точности дизайна</li>
+        {skills.map((skill, index) => (
+          <li key={index}>
+            <span className="skill">{skill}</span>
+          </li>
+        ))}
+       
       </ul>
     </div>
   );
